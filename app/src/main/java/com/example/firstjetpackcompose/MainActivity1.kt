03981806fdc95262e8ec4e3a45766432b0cba2e6
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-01 13:01:50 +0530

    Added image to column in NewsStory() method

--- app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
+++ app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -24,6 +26,7 @@ class MainActivity1 : ComponentActivity(){
 @Composable
 fun NewsStory(){
     Column(Modifier.padding(8.dp)) {
+        Image(painter = painterResource(id = R.drawable.image1), contentDescription = null)
         Text(text = "This is first text")
         Text(text = "Second")
         Text(text = "Third Text")
