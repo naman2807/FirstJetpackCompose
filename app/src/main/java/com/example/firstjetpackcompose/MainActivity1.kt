0a0575ea9c01ea1c5c261d03891caad1f276f0b2
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-01 13:05:52 +0530

    Added attributes to Image function.

--- app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
+++ app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
@@ -5,11 +5,11 @@ import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +26,9 @@ class MainActivity1 : ComponentActivity(){
 @Composable
 fun NewsStory(){
     Column(Modifier.padding(8.dp)) {
-        Image(painter = painterResource(id = R.drawable.image1), contentDescription = null)
+        Image(painter = painterResource(id = R.drawable.image1),
+            contentDescription = null, Modifier.height(180.dp).fillMaxWidth(),
+            contentScale = ContentScale.Crop)
         Text(text = "This is first text")
         Text(text = "Second")
         Text(text = "Third Text")
