a020f8f348b3a0b3f9d33fb4856ca83461fe525c
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-01 12:25:28 +0530

    Cleaned up the code.

--- app/src/main/java/com/example/firstjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstjetpackcompose/MainActivity.kt
@@ -13,26 +13,6 @@ import com.example.firstjetpackcompose.ui.theme.FirstJetpackComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FirstJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FirstJetpackComposeTheme {
-        Greeting("Android")
     }
 }
