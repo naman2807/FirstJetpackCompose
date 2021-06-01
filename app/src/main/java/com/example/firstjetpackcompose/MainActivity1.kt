abc054837635e8ea163c5da56681562aa719858c
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-01 12:57:10 +0530

    Added dp to column.

--- app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
+++ app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
@@ -5,9 +5,12 @@ import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity1 : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +23,7 @@ class MainActivity1 : ComponentActivity(){
 
 @Composable
 fun NewsStory(){
-    Column() {
+    Column(Modifier.padding(8.dp)) {
         Text(text = "This is first text")
         Text(text = "Second")
         Text(text = "Third Text")
