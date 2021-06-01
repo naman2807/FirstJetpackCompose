04f0f926d9ed4b20e48e07e003144b1d6e89b1a1
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-01 12:48:41 +0530

    Overridden the onCreate() method.

--- app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
+++ app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
@@ -1,6 +1,11 @@
 package com.example.firstjetpackcompose
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 
 class MainActivity1 : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
