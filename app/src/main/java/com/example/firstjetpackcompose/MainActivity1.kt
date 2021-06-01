b5824f1ffe1af11f3b3aebf45bc2965a5d462612
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-01 12:50:04 +0530

    Added new NewsStory() method.

--- app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
+++ app/src/main/java/com/example/firstjetpackcompose/MainActivity1.kt
@@ -3,9 +3,15 @@ package com.example.firstjetpackcompose
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
 
 class MainActivity1 : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+}
+
+@Composable
+fun NewsStory(){
+
 }
